6a41e80b6f71561bd8d2352d96bab93cb9083b0e
Oussamaosman02 <109099115+Oussamaosman02@users.noreply.github.com>
2023-03-08 09:10:24 +0000

    updating api

--- pages/api/index.js
+++ pages/api/index.js
@@ -3,11 +3,11 @@ import { Resp, mongoConn } from '../../utils/mongo.js'
 
 export async function handler (req, res) {
   console.log('Connecting for adding resp')
-  mongoConn()
+  await mongoConn()
   console.log('Connected, you can add an resp')
   const respu = req.body
-  const resp = await Resp.create({ respu })
+  const resp = await Resp.create(respu)
+  res.json({ resp })
   mongoose.connection.close()
   console.log('Closed connection, added resp')
-  res.json({ resp })
 }
